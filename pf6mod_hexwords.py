cd343f46f383f53f5f23c71676a00a88a1beed68
Will Urbanski <will.urbanski@gmail.com>
2012-03-28 22:14:59 -0400

    single character name support

--- pf6mod_hexwords.py
+++ pf6mod_hexwords.py
@@ -4,9 +4,12 @@ def plugin_main(*args, **kwargs):
     prefix = args[0]
     addrs = []
 
+    #A-F
+    hexchars = ["A","B","C","D","E","F"]
+    for hc in hexchars:
+        addrs.append("%s%s" % prefix, hc)
+        
 
-
-    print addrs
     return addrs
     
     
