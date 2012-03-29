21c17d987eba9d4e0050c54b55d90a2337196354
Will Urbanski <will.urbanski@gmail.com>
2012-03-28 21:59:12 -0400

    new dhcp server support
    added support for Jagornet and WIDE-DHCP default configs

--- pf6mod_dhcp.py
+++ pf6mod_dhcp.py
@@ -2,16 +2,27 @@
 
 #
 #   dhcp
-#   this checks for common dhcp patterns
 
 def plugin_main(*args, **kwargs):
     prefix = args[0]
     addrs = []
     
-    for i in range(1,1010):
+    for i in range(1,100):
         addrs.append("%s%i" % (prefix, i))
+    
+    #Jagornet DHCP Server v1.1 
+    #:2:1::/64
+    for i in range(10,255):
+        addrs.append("%s%s" % (prefix, hex(i)))
+        
+    #WIDE-DHCP
+    for i in range(1000,2000):
+        addrs.append("%s%s" % (prefix, i))
+    
     
     return addrs
     
+    
+    #2:0A-2:FF
     
     
